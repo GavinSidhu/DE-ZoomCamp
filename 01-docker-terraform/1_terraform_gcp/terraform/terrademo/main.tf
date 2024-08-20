0c81c05d192d3b73520f215f8950c36e3c08e653
GavinSidhu <gsidhu6666@gmail.com>
2024-08-20 19:39:36 -0400

    Terraform basics

---
+++ 01-docker-terraform/1_terraform_gcp/terraform/terrademo/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.42.0"
+    }
+  }
+}
+
+provider "google" {
+credentials = "./keys/mycreds.json"
+  project     = "theta-totem-432901-f9"
+  region      = "us-east1"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "gs-demo-bucket-de-zoomcamp"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
